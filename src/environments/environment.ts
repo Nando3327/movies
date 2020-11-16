18c39d433cbface2c4ac03e121f3d1e29acc1646
Fernando Castillo <fernando.castillo@easysoft.com.ec>
2020-11-16 09:51:05 -0500

    set language as english

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
     key: '1b501bbda107113acc653f328a2e935d',
     urlImages: 'https://image.tmdb.org/t/p/w440_and_h660_face/',
     moviesToShow: 10,
-    lang: 'es'
+    lang: 'en'
 };
 
 /*
