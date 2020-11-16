e50e6b8dbe63a8c8981796af09fc1ed3d2b77809
Fernando Castillo <fernando.castillo@easysoft.com.ec>
2020-11-16 09:48:25 -0500

    solve lint issues

--- src/app/favorites-movies/models/movie.model.ts
+++ src/app/favorites-movies/models/movie.model.ts
@@ -1,22 +1,31 @@
 export class DataMovie {
     popularity: number;
+    // tslint:disable-next-line:variable-name
     vote_count: number;
     video: boolean;
+    // tslint:disable-next-line:variable-name
     poster_path: string;
     id: number;
     adult: boolean;
+    // tslint:disable-next-line:variable-name
     backdrop_path: string;
+    // tslint:disable-next-line:variable-name
     original_language: string;
+    // tslint:disable-next-line:variable-name
     original_title: string;
     title: string;
+    // tslint:disable-next-line:variable-name
     vote_average: number;
     overview: string;
+    // tslint:disable-next-line:variable-name
     release_date: string;
 }
 
 export class DataMovieResponse {
     page: number;
+    // tslint:disable-next-line:variable-name
     total_results: number;
+    // tslint:disable-next-line:variable-name
     total_pages: number;
     results: Array<DataMovie>;
 }
